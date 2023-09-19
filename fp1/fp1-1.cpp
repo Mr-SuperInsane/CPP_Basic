4afdd6e80e7a1ddf64cdedf83c5eb4c74ee579a6
INSANE <70059960+Mr-SuperInsane@users.noreply.github.com>
2023-09-19 14:26:43 +0900

    ver2023/9/19

---
+++ fp1/fp1-1.cpp
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+int main(void) {
+	int i = 0, sum = 0;
+	int test[3];
+
+	printf("3科目のテストの点数を入力してください \n");
+	for (i = 0; i < 3; i++) {
+		scanf("%d", &test[i]);
+	}
+	for (i = 0; i < 3; i++) {
+		sum += test[i];
+	}
+	sum /= 3;
+	if (sum >= 60) {
+		printf("合格");
+	}
+	else {
+		printf("不合格");
+	}
+}
+
+// 30 50 70
