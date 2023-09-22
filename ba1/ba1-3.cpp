42faf73902ac7cdfa038951e6441d70a2c0036bf
INSANE <70059960+Mr-SuperInsane@users.noreply.github.com>
2023-09-22 15:37:15 +0900

    ver2023/9/22

---
+++ ba1/ba1-3.cpp
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+
+void triangle(int num) {
+	int i, j;
+	for (i = 0; i < num; i++) {
+		for (j = 0; j < num-i-1; j++) {
+			printf(" ");
+		}
+		for (j = 0; j < i+1; j++) {
+			printf("*");
+		}
+		printf("\n");
+
+	}
+}
+
+int main(void) {
+	int num;
+	printf("三角形の縦サイズ：");
+	scanf("%d", &num);
+	triangle(num);
+	return 0;
+}
