197dd71785ef1903e6dc983a3598eaa4f12b5b12
INSANE <70059960+Mr-SuperInsane@users.noreply.github.com>
2023-09-22 14:31:25 +0900

    ver2023/9/22

---
+++ ba1/ba1-1.cpp
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+
+void sum(void) {
+	int i;
+	int result = 0;
+	for (i = 0; i < 100; i++) {
+		result += i + 1;
+	}
+	printf("%d", result);
+}
+
+int main(void) {
+	sum();
+	return 0;
+}
---
+++ ba1/ba1-2.cpp
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+
+void kuku(void) {
+    int i, j;
+
+    for (i = 1; i <= 9; i++) {
+        for (j = 1; j <= 9; j++) {
+            printf("%d\t", i * j);
+        }
+        printf("\n");
+    }
+}
+
+int main(void) {
+    kuku();
+    return 0;
+}
